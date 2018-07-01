75c6b135c3f2f89e58028d360d6785492dcc5c6c
Anton Samoilov <absamoilov88@gmail.com>
2018-07-01 18:14:46 +0300

    Tune jwt login

--- router.js
+++ router.js
@@ -1,5 +1,12 @@
 const Auth = require('./controllers/auth');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', { session: false });
 
 module.exports = (app) => {
+  app.get('/', requireAuth, (req, res) => {
+    res.send({ msg: 'Test' });
+  });
   app.post('/signup', Auth.signup);
 }
