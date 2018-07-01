81d7a2376db5fa4081f5d29b4b9310144131ed4c
Anton Samoilov <absamoilov88@gmail.com>
2018-07-01 15:52:35 +0300

    Setup routing

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const router = require('./router');
 
 app.use(morgan('combined'));
+
+router(app);
 
 const port = process.env.PORT || 3090;
 
