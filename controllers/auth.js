ef2a608bf63b9c1af22ae75359ee69689b5797c1
Anton Samoilov <absamoilov88@gmail.com>
2018-07-01 16:40:21 +0300

    Create signup action

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,23 @@
+const User = require('../models/user');
+
 exports.signup = (req, res, next) => {
-  res.send({ success: 'true' });
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email }, (err, exUser) => {
+    if (exUser) {
+      return res.status(422).send({ error: 'User with this email is already registered '});
+    }
+
+    const user = new User({
+      email,
+      password
+    });
+
+    user.save(err => {
+      if (err) return next(err);
+
+      res.json(user);
+    });
+  });
 }
