1a0a0ce3052198c18820bca994e2887ae87825ac
Anton Samoilov <absamoilov88@gmail.com>
2018-07-01 16:41:13 +0300

    Include body-parser to parse requests

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 const router = require('./router');
@@ -7,6 +8,7 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/auth');
 
 app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
 const port = process.env.PORT || 3090;
